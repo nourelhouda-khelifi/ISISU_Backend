8db9c36bb2e739375d06fecf480f938eff6991d1
nourelhouda-khelifi <123408799+nourelhouda-khelifi@users.noreply.github.com>
2026-03-22 20:09:41 +0100

    fix: ajouter SecurityScheme à la configuration OpenAPI pour afficher le bouton Authorize dans Swagger

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 
 @Configuration
 public class OpenApiConfig {
@@ -22,6 +24,13 @@ public class OpenApiConfig {
                                 .email("support@isisu.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
     }
 }
